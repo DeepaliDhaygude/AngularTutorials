9bb841d461fd13c7e66e692ce43a6901485b43e8
Deepali <you@example.com>
2023-04-20 01:21:12 +0530

    First Commit

---
+++ AngularTutorials/src/app/templateform1/templateform1.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Templateform1Component } from './templateform1.component';
+
+describe('Templateform1Component', () => {
+  let component: Templateform1Component;
+  let fixture: ComponentFixture<Templateform1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Templateform1Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Templateform1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ AngularTutorials/src/app/templateform1/templateform1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templateform1',
+  templateUrl: './templateform1.component.html',
+  styleUrls: ['./templateform1.component.scss']
+})
+export class Templateform1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
